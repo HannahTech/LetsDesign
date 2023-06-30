56d84b51b17378e7d29be4dae1ded635bb45254f
Hannah <hannah.ai.arch@gmail.com>
2023-06-30 01:30:03 -0400

    empty

--- script.js
+++ script.js
@@ -84,14 +84,6 @@ window.addEventListener("DOMContentLoaded", function () {
     element.style.transform = "translateX(" + position + "%)";
   }
 
-  function isElementInViewport(element) {
-    const rect = element.getBoundingClientRect();
-    return (
-      (rect.top >= 0 && rect.top + 250 <= window.innerHeight) ||
-      (rect.bottom >= 0 && rect.bottom + 250 <= window.innerHeight)
-    );
-  }
-
   window.addEventListener("scroll", toggleStickyMenu);
 });
 
@@ -100,8 +92,8 @@ const verticalParts = document.querySelectorAll(".vertical-part");
 const responsibleWidth = 950;
 const luxuryImage = document.querySelector(".luxury-image");
 const containerWidth = luxuryImage.offsetWidth;
-const initialPosition = containerWidth - window.innerWidth;
-const scrollImage = window.innerWidth > responsibleWidth ? 35 : 45;
+const initialPosition = containerWidth - window.innerWidth + 15;
+const scrollImage = window.innerWidth > responsibleWidth ? 60 : 45;
 
 verticalParts.forEach((part) => {
   part.addEventListener("mouseenter", () => {
@@ -122,6 +114,64 @@ verticalParts.forEach((part) => {
     });
   });
 });
+
+var originalImages = [
+  "url(images/heto/1.jpg)",
+  "url(images/heto/2.jpg)",
+  "url(images/heto/3.jpg)",
+  "url(images/heto/4.jpg)",
+  "url(images/heto/5.jpg)",
+  "url(images/heto/6.jpg)",
+  "url(images/heto/6.jpg)",
+  "url(images/heto/5.jpg)",
+  "url(images/heto/4.jpg)",
+  "url(images/heto/3.jpg)",
+  "url(images/heto/2.jpg)",
+  "url(images/heto/1.jpg)",
+];
+
+var imageSelectors = [
+  ":nth-child(1)",
+  ":nth-child(2)",
+  ":nth-child(3)",
+  ":nth-child(4)",
+  ":nth-child(5)",
+  ":nth-child(6)",
+  ":nth-child(7)",
+  ":nth-child(8)",
+  ":nth-child(9)",
+  ":nth-child(10)",
+  ":nth-child(11)",
+  ":nth-child(12)",
+];
+
+function hoverImages() {
+  var images = document.querySelectorAll(".several-projects .image");
+  var delay = 250;
+
+  for (var i = 0; i < images.length; i++) {
+    (function (index) {
+      setTimeout(function () {
+        var imageName = originalImages[index].split("/").pop().split(".")[0];
+
+        images[index].style.backgroundImage =
+          "url(images/heto/" + imageName + "-2.jpg)";
+      }, delay * index);
+    })(i);
+  }
+}
+
+function resetImages() {
+  var images = document.querySelectorAll(".several-projects .image");
+  for (var i = 0; i < images.length; i++) {
+    images[i].style.backgroundImage = originalImages[i];
+  }
+}
+
+var images = document.querySelectorAll(".several-projects .image");
+for (var i = 0; i < images.length; i++) {
+  images[i].classList.add("image" + imageSelectors[i]);
+}
 
 window.onbeforeunload = function () {
   window.scrollTo(0, 0);
